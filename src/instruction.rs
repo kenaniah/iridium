4c32c21fa0e6e424cf5f230b0fd865bacbf47ee9
Kenaniah Cerny <kenaniah@gmail.com>
2020-03-29 13:32:03 -0700

    checkpoint

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug, PartialEq)]
 #[allow(non_camel_case_types)]
 pub enum Opcode {
@@ -97,6 +99,164 @@ pub enum Opcode {
     INVALID = 255,
 }
 
+#[derive(Default)]
+pub struct OpcodeArity {
+    argc: u8,
+    arg1_size: u8,
+    arg1_signed: bool,
+    arg2_size: u8,
+    arg2_signed: bool,
+    arg3_size: u8,
+    arg3_signed: bool,
+}
+
+impl Opcode {
+    fn arity(&self) -> OpcodeArity {
+        match self {
+            // No args
+            Self::NOP
+            | Self::EXT1
+            | Self::EXT2
+            | Self::EXT3
+            | Self::STOP
+            | Self::MAX
+            | Self::INVALID => OpcodeArity {
+                argc: 0,
+                ..OpcodeArity::default()
+            },
+            // u8
+            Self::LOADI_0
+            | Self::LOADI_1
+            | Self::LOADI_2
+            | Self::LOADI_3
+            | Self::LOADNIL
+            | Self::LOADSELF
+            | Self::LOADT
+            | Self::LOADF
+            | Self::EXCEPT
+            | Self::POPERR
+            | Self::RAISE
+            | Self::EPUSH
+            | Self::EPOP
+            | Self::CALL
+            | Self::RETURN
+            | Self::RETURN_BLK
+            | Self::BREAK
+            | Self::ARYCAT
+            | Self::ARYPUSH
+            | Self::STRCAT
+            | Self::RANGE_INC
+            | Self::RANGE_EXC
+            | Self::OCLASS
+            | Self::ALIAS
+            | Self::SCLASS
+            | Self::TCLASS
+            | Self::ERR => OpcodeArity {
+                argc: 1,
+                arg1_size: 8,
+                ..OpcodeArity::default()
+            },
+            // u16
+            Self::JMP => OpcodeArity {
+                argc: 1,
+                arg1_size: 16,
+                ..OpcodeArity::default()
+            },
+            // i16
+            Self::ONERR => OpcodeArity {
+                argc: 1,
+                arg1_size: 16,
+                arg1_signed: true,
+                ..OpcodeArity::default()
+            },
+            // u24
+            Self::ENTER => OpcodeArity {
+                argc: 1,
+                arg1_size: 24,
+                ..OpcodeArity::default()
+            },
+            // u8, u8
+            Self::MOVE
+            | Self::LOADL
+            | Self::LOADSYM
+            | Self::GETGV
+            | Self::SETGV
+            | Self::GETSV
+            | Self::SETSV
+            | Self::GETIV
+            | Self::SETIV
+            | Self::GETCV
+            | Self::SETCV
+            | Self::GETCONST
+            | Self::SETCONST
+            | Self::GETMCNST
+            | Self::SETMCNST
+            | Self::RESCUE
+            | Self::SENDV
+            | Self::SENDVB
+            | Self::SUPER
+            | Self::KARG
+            | Self::KARG2
+            | Self::ADD
+            | Self::SUB
+            | Self::SUBI
+            | Self::MUL
+            | Self::DIV
+            | Self::EQ
+            | Self::LT
+            | Self::LE
+            | Self::GT
+            | Self::GE
+            | Self::ARRAY
+            | Self::ARRAY2
+            | Self::AREF
+            | Self::ASET
+            | Self::APOST
+            | Self::STRING
+            | Self::HASH
+            | Self::HASHADD
+            | Self::LAMBDA
+            | Self::BLOCK
+            | Self::METHOD
+            | Self::CLASS
+            | Self::MODULE
+            | Self::EXEC
+            | Self::DEF
+            | Self::UNDEF => OpcodeArity {
+                argc: 2,
+                arg1_size: 8,
+                arg2_size: 8,
+                ..OpcodeArity::default()
+            },
+            // u8, i8
+            Self::LOADI => OpcodeArity {
+                argc: 2,
+                arg1_size: 8,
+                arg2_size: 8,
+                arg2_signed: true,
+                ..OpcodeArity::default()
+            },
+            // u8, u16
+            Self::JMPIF | Self::JMPNOT | Self::ARGARY | Self::BLKPUSH => OpcodeArity {
+                argc: 2,
+                arg1_size: 8,
+                arg2_size: 16,
+                ..OpcodeArity::default()
+            },
+            // u8, u8, u8
+            Self::GETUPVAR | Self::SETUPVAR | Self::SEND | Self::SENDB | Self::ADDI => {
+                OpcodeArity {
+                    argc: 3,
+                    arg1_size: 8,
+                    arg2_size: 8,
+                    arg3_size: 8,
+                    ..OpcodeArity::default()
+                }
+            }
+        }
+    }
+}
+
 pub type U24 = (u8, u8, u8);
 
 #[derive(Debug, PartialEq)]
@@ -112,7 +272,6 @@ pub enum InstructionArgs {
     // Two arguments
     U8U8(u8, u8),
     U8I8(u8, i8),
-    U16U8(u16, u8),
     U8U16(u8, u16),
     U8I16(u8, i16),
     U16U16(u16, u16),
